d006f983fcd793f967ca77f85f8ad6b183b33d10
pattryk95 <patryk.bogdanski95@gmail.com>
2023-12-02 00:31:21 +0100

    Refactor code by adding functions to check_event

--- game_functions.py
+++ game_functions.py
@@ -3,21 +3,27 @@ import sys
 import pygame as pg
 
 
+def check_keydown_events(event, ship):
+    if event.key == pg.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pg.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pg.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pg.K_LEFT:
+        ship.moving_left = False
+
+
 def check_event(ship):
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pg.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pg.KEYUP:
-            if event.key == pg.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pg.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
