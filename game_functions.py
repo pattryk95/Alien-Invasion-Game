1916af3632878c8e9a9666d7469d339d671a73b5
pattryk95 <patryk.bogdanski95@gmail.com>
2023-12-02 23:23:36 +0100

    Add separate function to firing bulllets

--- game_functions.py
+++ game_functions.py
@@ -10,9 +10,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pg.K_LEFT:
         ship.moving_left = True
     elif event.key == pg.K_SPACE:
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -46,3 +44,8 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
