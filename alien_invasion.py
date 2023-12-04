bde742b5cfd55b4fa9d3937fb5d3b60987f29bc6
pattryk95 <patryk.bogdanski95@gmail.com>
2023-12-04 23:05:58 +0100

    Add Group of Alients to games

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 from alien import Alien
-from game_functions import check_events, update_screen, update_bullets
+from game_functions import check_events, update_screen, update_bullets, create_fleet
 
 
 def run_game():
@@ -13,15 +13,17 @@ def run_game():
     screen = pg.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pg.display.set_caption("Inwacja Obcych")
     ship = Ship(ai_settings, screen)
-    alien = Alien(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    create_fleet(ai_settings, screen, aliens)
 
     while True:
         check_events(ai_settings, screen, ship, bullets)
         ship.update()
         update_bullets(bullets)
 
-        update_screen(ai_settings, screen, ship, alien, bullets)
+        update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame as pg
 from bullet import Bullet
+from alien import Alien
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -32,13 +33,13 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
 
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
     pg.display.flip()
 
 
@@ -53,3 +54,16 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+
+def create_fleet(ai_settings, screen, aliens):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    for alien_number in range(number_aliens_x):
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
